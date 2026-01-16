a7d2798c6936a18b25b7abc522c37c36fd9dade1
yvvw <g592842897@gmail.com>
2026-01-17 02:24:02 +0800

    openwrt

---
+++ openwrt-builder/README.md
@@ -0,0 +1,11 @@
+https://github.com/immortalwrt/immortalwrt
+
+```sh
+sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.org/init_build_environment.sh)'
+
+git clone -b openwrt-25.12 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
+
+# copy builder folder & custom target settings
+
+BUILD_TARGET=target bash builder/build.sh
+```
---
+++ openwrt-builder/shared/diy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# patch diy
+if [ -d "${BUILDER_DIR}/shared/patchs" ]; then
+	batch_patch "${BUILDER_DIR}/shared/patchs"/diy_*.patch
+fi
+if [ -d "${BUILDER_DIR}/targets/${BUILD_TARGET}/patchs" ]; then
+	batch_patch "${BUILDER_DIR}/targets/${BUILD_TARGET}/patchs"/diy_*.patch
+fi
+
+# kernel patch
+find "target/linux" -type f -path "target/linux/*/config-*" | while read -r config; do
+	if ! grep -q 'CONFIG_FAT_DEFAULT_IOCHARSET="utf8"' "$config"; then
+		sed -i '$a \
+CONFIG_FAT_DEFAULT_IOCHARSET="utf8" \
+CONFIG_CRYPTO_CHACHA20_NEON=y \
+CONFIG_CRYPTO_CHACHA20POLY1305=y' "$config"
+	fi
+done
+
+# patch tcp bbr
+if grep -qE "bbr=y|turboacc=y" .config && [ ! -d "package/turboacc" ]; then
+	curl -sSL "https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh" | bash
+fi
+
+# custom uci-defaults
+UCI_TARGET="package/base-files/files/etc/uci-defaults"
+rm -rf "${UCI_TARGET}" && git restore "${UCI_TARGET}"
+copy_uci() {
+	uci_dir="$1"
+	target="$2"
+	[ ! -d "${uci_dir}" ] && return 0
+	for fp in "${uci_dir}"/*; do
+		[ -f "$fp" ] || continue
+		fn="${fp##*/}"
+		cp "$fp" "${UCI_TARGET}/${fn}-${target}"
+	done
+}
+copy_uci "${BUILDER_DIR}/shared/files/etc/uci-defaults" "shared"
+copy_uci "${BUILDER_DIR}/targets/${BUILD_TARGET}/files/etc/uci-defaults" "${BUILD_TARGET}"
+
+for name in ${!M_@}; do
+	value="${!name}"
+	sed -i "s,<${name}>,${value},g" "${UCI_TARGET}"/*
+done
