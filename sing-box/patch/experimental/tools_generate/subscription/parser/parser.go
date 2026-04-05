debdf7fd58e545c7dc8f4b29a184d27a8a8cfd8a
yvvw <15168529+yvvw@users.noreply.github.com>
2026-04-05 12:12:02 +0800

    parser

--- sing-box/patch/experimental/tools_generate/subscription/parser/parser.go
+++ sing-box/patch/experimental/tools_generate/subscription/parser/parser.go
@@ -3,8 +3,10 @@ package parser
 import (
 	"context"
 
+	"github.com/sagernet/sing-box/include"
 	"github.com/sagernet/sing-box/option"
 	E "github.com/sagernet/sing/common/exceptions"
+	"github.com/sagernet/sing/service"
 )
 
 var subscriptionParsers = []func(ctx context.Context, content string) ([]option.Outbound, error){
@@ -15,9 +17,10 @@ var subscriptionParsers = []func(ctx context.Context, content string) ([]option.
 }
 
 func ParseSubscription(ctx context.Context, content string) ([]option.Outbound, error) {
+	pctx := service.ContextWithDefaultRegistry(include.Context(ctx))
 	var pErr error
 	for _, parser := range subscriptionParsers {
-		servers, err := parser(ctx, content)
+		servers, err := parser(pctx, content)
 		if len(servers) > 0 {
 			return servers, nil
 		}
